8764179df6c7d660202737f117bf7018069d5f74
David Yin <david.yin@austinisd.org>
2019-11-19 08:40:48 -0600

    Starter Code

---
+++ src/Fractal.java
@@ -0,0 +1,18 @@
+import gpdraw
+
+import static java.lang.Math.PI;
+
+public class Fractal {
+    SketchPad pad=new SketchPad(500,500);
+    DrawingTool pen=new DrawingTool(pad);
+    public Fractal(){};
+    public void move(int length){
+//   Your code here
+    }
+    public void direction(){
+//   Your code here
+    }
+    public void curve(int level, double length){
+//   Your code here
+    }
+}
